d4768320a6d816915a8bd2ba46e034379e773721
Alfredleo7 <stalgonz@espol.edu.ec>
2017-08-27 12:45:45 -0500

    totales en el reporte de operario

--- public/app_Angular/General/controllers/reporte.controller.js
+++ public/app_Angular/General/controllers/reporte.controller.js
@@ -25,20 +25,35 @@ angular.module('general').controller('reporte.controller', ['$scope','$http',
         method: 'GET',
         url: '/api/DetallesBySucursal/'+$scope.filtroAnio
       }).then(function(response){
-        console.log(response.data);
+        var totalCategoria;
+        var indice;
+        var _valor;
+        $scope.totalMeses = new Array(12).fill(0);
+        //Darle un formato manejable
         for(var i in response.data){
           categoria = {
             descripcion: response.data[i]._id,
-            meses: new Array(12)
+            meses: new Array(13)
           };
+          totalCategoria = 0;
+
+          //Enviar cada valor al mes correspondiente
           for(var j in response.data[i].meses){
-            categoria.meses[response.data[i].meses[j].mes-1] = {
-              valor: response.data[i].meses[j].total
+            indice = response.data[i].meses[j].mes-1;
+            _valor = response.data[i].meses[j].total;
+            categoria.meses[indice] = {
+              valor: _valor
             };
+            totalCategoria += _valor;
+            $scope.totalMeses[indice] += _valor;
           }
+          //Calcular el total de la categoria
+          categoria.meses[12] = {
+            valor: totalCategoria
+          };
+          //Agregar a la variable
           $scope.categorias.push(categoria);
         }
-        console.log($scope.categorias);
       }, function(errorResponse){
         console.log(errorResponse.data);
       })
